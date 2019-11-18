145f650fd166ef2c442d4c2e698acd85f7e7cd89
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-11-18 17:57:23 +0000

    Add files via upload

---
+++ Par ou Impar/Par.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_ou_Impar
+{
+    class Par
+    {
+        private int valor;
+        public string recebe
+        { 
+            set
+            {
+                valor = Convert.ToInt32(value);
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                bool primo = true;
+                for (int i = 2; i < valor; i++) 
+                {
+                    if (valor % i == 0) primo = false;
+                }
+                if (primo) return "O número " + valor + " é Primo";
+                else return "O número " + valor + " não é Primo";
+
+            }
+        }
+    }
+}
